cd1ca1312540322039304a9e4f05472b0a26bd41
Yang JianXin <995462226@qq.com>
2023-08-18 15:32:58 +0800

    在cpu中合并权重，缓解显存不足导致的合并问题

--- script/merge_lora.py
+++ script/merge_lora.py
@@ -23,9 +23,10 @@ def merge_lora_to_base_model():
         trust_remote_code=True,
         low_cpu_mem_usage=True,
         torch_dtype=torch.float16,
-        device_map='auto'
+        # device_map='auto',
+        device_map={'': 'cpu'}
     )
-    model = PeftModel.from_pretrained(model, adapter_name_or_path)
+    model = PeftModel.from_pretrained(model, adapter_name_or_path, device_map={'': 'cpu'})
     model = model.merge_and_unload()
 
     tokenizer.save_pretrained(save_path)
