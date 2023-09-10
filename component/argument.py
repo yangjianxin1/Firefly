d53c1c7e34bf448c96f9d80c29071307e87073b1
Lucien <lucien@lucien.ink>
2023-09-10 16:24:52 +0800

    fix typo in argument.py

--- component/argument.py
+++ component/argument.py
@@ -22,7 +22,7 @@ class QLoRAArguments:
     train_file: str = field(metadata={"help": "训练集"})
     model_name_or_path: str = field(metadata={"help": "预训练权重路径"})
     task_type: str = field(default="", metadata={"help": "预训练任务：[sft, pretrain]"})
-    eval_file: Optional[str] = field(default="", metadata={"help": "the file of training data"})
+    eval_file: Optional[str] = field(default="", metadata={"help": "the file of evaluate data"})
     lora_rank: Optional[int] = field(default=64, metadata={"help": "lora rank"})
     lora_alpha: Optional[int] = field(default=16, metadata={"help": "lora alpha"})
     lora_dropout: Optional[float] = field(default=0.05, metadata={"help": "lora dropout"})
