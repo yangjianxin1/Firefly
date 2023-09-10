ab154307d5e5f3e9ce0549d0c90783f92b87c883
Lucien <lucien@lucien.ink>
2023-09-10 16:27:28 +0800

    Update argument.py

--- component/argument.py
+++ component/argument.py
@@ -10,7 +10,7 @@ class CustomizedArguments:
     max_seq_length: int = field(metadata={"help": "输入最大长度"})
     train_file: str = field(metadata={"help": "训练集"})
     model_name_or_path: str = field(metadata={"help": "预训练权重路径"})
-    eval_file: Optional[str] = field(default="", metadata={"help": "the file of training data"})
+    eval_file: Optional[str] = field(default="", metadata={"help": "the evaluate data file"})
 
 
 @dataclass
@@ -22,7 +22,7 @@ class QLoRAArguments:
     train_file: str = field(metadata={"help": "训练集"})
     model_name_or_path: str = field(metadata={"help": "预训练权重路径"})
     task_type: str = field(default="", metadata={"help": "预训练任务：[sft, pretrain]"})
-    eval_file: Optional[str] = field(default="", metadata={"help": "the file of evaluate data"})
+    eval_file: Optional[str] = field(default="", metadata={"help": "the evaluate data file"})
     lora_rank: Optional[int] = field(default=64, metadata={"help": "lora rank"})
     lora_alpha: Optional[int] = field(default=16, metadata={"help": "lora alpha"})
     lora_dropout: Optional[float] = field(default=0.05, metadata={"help": "lora dropout"})
